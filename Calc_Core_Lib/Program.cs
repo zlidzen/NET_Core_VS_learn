0ec7e047fc49f44e4972eb7e15ab407107eb7da8
zlidzen <m.karsakou@gmail.com>
2022-05-13 11:40:48 +0300

    init Calc sln

---
+++ Calc_Core_Lib/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calc_Core_Lib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
